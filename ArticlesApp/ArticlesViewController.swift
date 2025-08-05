d30ec2d3226af7d9504dd4c7cb0fd573efa97757
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2025-08-05 13:20:34 +0200

    added UITabBarController

---
+++ ArticlesApp/ArticlesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ArticlesViewController.swift
+//  ArticlesApp
+//
+//  Created by Lora Zubic on 05.08.2025..
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .yellow
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ArticlesApp/FavoritesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesViewController.swift
+//  ArticlesApp
+//
+//  Created by Lora Zubic on 05.08.2025..
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
