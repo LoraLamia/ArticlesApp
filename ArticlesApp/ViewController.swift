f0c63889c0f970a52926959bb9fa4338cfb647bf
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2025-08-05 12:09:27 +0200

    inital commit

---
+++ ArticlesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArticlesApp
+//
+//  Created by Lora Zubic on 05.08.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
