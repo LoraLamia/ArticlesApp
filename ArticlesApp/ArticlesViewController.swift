410ee41f0cc0d9328969a7ad86985bd54d6c8fd0
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2025-08-06 15:31:30 +0200

    search enabled

--- ArticlesApp/ArticlesViewController.swift
+++ ArticlesApp/ArticlesViewController.swift
@@ -19,7 +19,9 @@ class ArticlesViewController: UIViewController {
     let articlesTableView = UITableView()
     let searchTextField = UITextField()
     var articles: [Article] = []
-    //    var mockArticles = Article.sampleData
+    var filteredArticles: [Article] = []
+    var searchWorkItem: DispatchWorkItem?
+    
     let headers: HTTPHeaders = [
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4OTM0OGJiMWUwNjM0NmZhYjE1NDc4NyIsInVzZXJuYW1lIjoiam9obi5kb2UudGhpcmQiLCJyb2xlIjoiQmFzaWMiLCJpYXQiOjE3NTQ0ODI4NzUsImV4cCI6MTc1NDQ5MzY3NX0.cSW4ZORedw2_2qppbYgLT01tH9HvwEEVjIPYkA3vqA0"
     ]
@@ -52,7 +54,32 @@ class ArticlesViewController: UIViewController {
     }
     
     @objc private func searchTextFieldChanged() {
+        searchWorkItem?.cancel()
         
+        let searchText = searchTextField.text?.lowercased() ?? ""
+        
+        searchWorkItem = DispatchWorkItem { [weak self] in
+            guard let self = self else { return }
+            
+            if searchText.isEmpty {
+                self.filteredArticles = self.articles
+            } else {
+                self.filteredArticles = self.articles.filter { article in
+                    
+                    let title = article.title.lowercased().contains(searchText)
+                    let summary = article.summary.lowercased().contains(searchText)
+                    let author = article.author.lowercased().contains(searchText)
+                    return title || summary || author
+                }
+            }
+            
+            DispatchQueue.main.async {
+                self.articlesTableView.reloadData()
+            }
+            
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: searchWorkItem!)
     }
     
     private func setupTableView() {
@@ -108,6 +135,8 @@ class ArticlesViewController: UIViewController {
         }
         
         self.articles.append(contentsOf: moreArticles)
+        self.filteredArticles = self.articles
+        
         self.currentPage += 1
         self.articlesTableView.reloadData()
     }
@@ -123,11 +152,11 @@ class ArticlesViewController: UIViewController {
 
 extension ArticlesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        articles.count
+        filteredArticles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let article = articles[indexPath.row]
+        let article = filteredArticles[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticlesTableViewCell", for: indexPath) as! ArticlesTableViewCell
         cell.delegate = self
         cell.configure(article: article)
